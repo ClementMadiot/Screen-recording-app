d61eaa3f2bd0b9ed096ed5d23b40d6b7f08e01a2
ClementMadiot <clementmadiot09@gmail.com>
2025-06-06 11:24:12 +0200

    setup Bunny for video Upload

--- constants/index.ts
+++ constants/index.ts
@@ -2,11 +2,11 @@ export const MAX_VIDEO_SIZE = 500 * 1024 * 1024;
 export const MAX_THUMBNAIL_SIZE = 10 * 1024 * 1024;
 
 export const BUNNY = {
-  STREAM_BASE_URL: "https://video.bunnycdn.com/library",
-  STORAGE_BASE_URL: "https://sg.storage.bunnycdn.com/snapcast",
-  CDN_URL: "https://snapcast.b-cdn.net",
+  STORAGE_BASE_URL: "https://storage.bunnycdn.com/snapcast-cm",
+  CDN_URL: "https://snapcast-cm.b-cdn.net",
+  TRANSCRIPT_URL: "https://vz-a70cba6c-281.b-cdn.net",
   EMBED_URL: "https://iframe.mediadelivery.net/embed",
-  TRANSCRIPT_URL: "https://vz-47a08e64-84d.b-cdn.net",
+  STREAM_BASE_URL: "https://video.bunnycdn.com/library",
 };
 
 export const emojis = ["😂", "😍", "👍"];
@@ -19,6 +19,7 @@ export const filterOptions = [
 ];
 
 export const visibilities: Visibility[] = ["public", "private"];
+
 
 export const ICONS = {
   record: "/assets/icons/record.svg",
@@ -135,5 +136,5 @@ export const dummyCards = [
     views: 67,
     visibility: "public",
     duration: 345,
-  }
-]
+  },
+];
